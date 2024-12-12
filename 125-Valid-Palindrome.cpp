f80b78a8547ac0d9e14a5def0bd47eb8bfdae1f8
Mohammed Sameer A <149071470+MOHAMMED-SAMEE@users.noreply.github.com>
2024-12-12 12:19:50 +0530

    [125] [Time Beats: 100.00%] [Memory Beats: 6.73%] - LeetPush

--- 125-Valid-Palindrome.cpp
+++ 125-Valid-Palindrome.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        int l = 0, r = s.size() - 1;
-
-        while (l < r) {
-            while (l < r && !isalnum(s[l])) l++;
-            while (l < r && !isalnum(s[r])) r--;
-
-            if (tolower(s[l]) != tolower(s[r])) {
-                return false;
+        string m;
+        for(int i=0;i<s.size();i++){
+            m+=tolower(s[i]);
+        }
+        string r;
+        string n;
+        for(int i=0;i<m.size();i++){
+            if(m[i]>='a' && m[i]<='z' || m[i]>='0' && m[i]<='9'){
+                n+=m[i];
             }
-
-            l++;
-            r--;
+        }
+        for(int i=n.size()-1;i>=0;i--){
+            r+=n[i];
         }
 
-        return true;
+    
+        if(n==r){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 };
