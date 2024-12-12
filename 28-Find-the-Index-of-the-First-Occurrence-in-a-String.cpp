96116d0f3e3ca187bb73d48197e3c88716aac647
Mohammed Sameer A <149071470+MOHAMMED-SAMEE@users.noreply.github.com>
2024-12-12 11:40:49 +0530

    [28] [Time Beats: 100.00%] [Memory Beats: 8.18%] - LeetPush

---
+++ 28-Find-the-Index-of-the-First-Occurrence-in-a-String.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int count=0;
+        if (haystack.length() < needle.length()) {
+            return -1;
+        }
+        for(int i=0;i<=haystack.size()-needle.size();i++){
+            
+            if(haystack.substr(i,needle.size())==needle){
+                return i;
+            }
+                
+            
+        }
+        return -1;
+        
+    }
+};
