edb996b9cf497ecba0bb23bb7b3e8b63c7455e7c
Mohammed Sameer A <149071470+MOHAMMED-SAMEE@users.noreply.github.com>
2024-12-24 14:17:38 +0530

    [219] [Time Beats: 87.22%] [Memory Beats: 38.98%] - LeetPush

---
+++ 219-Contains-Duplicate-II.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+       unordered_map<int,int> n;
+        for(int i=0;i<nums.size();i++){
+            int val=nums[i];
+            if(n.find(val)!=n.end() && i- n[val]<=k){
+                return true;
+            }
+            n[val]=i;
+        }
+        return false;
+        
+    }
+};
