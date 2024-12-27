a6c236f828c88e9d292c71cca35e6fb9026d85b0
Mohammed Sameer A <149071470+MOHAMMED-SAMEE@users.noreply.github.com>
2024-12-27 11:23:19 +0530

    [92] [Time Beats: 100.00%] [Memory Beats: 17.59%] - LeetPush

---
+++ 92-Reverse-Linked-List-II.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+        vector<int> num;
+        while(head!=NULL){
+            num.push_back(head->val);
+            head=head->next;
+        }
+        reverse(num.begin()+(left-1),num.begin()+(right));
+        ListNode* temp=new ListNode;
+        ListNode* dum=temp;
+        for(int i=0;i<num.size();i++){
+            dum->next=new ListNode(num[i]);
+            dum=dum->next;
+        }
+        return temp->next;
+    }
+        
+    
+};
