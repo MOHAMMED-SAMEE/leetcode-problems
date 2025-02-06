128cd0cf907001393034c37342eef9e6421184f7
Mohammed Sameer A <149071470+MOHAMMED-SAMEE@users.noreply.github.com>
2025-02-06 15:56:11 +0530

    [3083] [Time Beats: 100.00%] [Memory Beats: 77.54%] - LeetPush

---
+++ 3083-Existence-of-a-Substring-in-a-String-and-Its-Reverse.cpp
@@ -0,0 +1,27 @@
+#pragma GCC optimize("O3", "unroll-loops")
+class Solution {
+public:
+  bool isSubstringPresent(string s) {
+    for (int i = 0; i < s.size() - 1; i++) {
+    
+      string substring = s.substr(i, 2);
+
+    
+      reverse(substring.begin(), substring.end());
+
+      
+      if (s.find(substring) != string::npos) {
+        return true;
+      }
+    }
+
+
+    return false;
+  }
+};
+auto init = []() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    return 'c';
+}();
