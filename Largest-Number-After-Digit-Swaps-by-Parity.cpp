7e2e5e8f8d7b893416657c137b2ab7e0f8f6ae36
Mohammed Sameer A <149071470+MOHAMMED-SAMEE@users.noreply.github.com>
2025-03-03 13:04:00 +0530

    [2231] [Time Beats: 100.00%] [Memory Beats: 58.64%] - LeetPush

---
+++ Largest-Number-After-Digit-Swaps-by-Parity.cpp
@@ -0,0 +1,21 @@
+class Solution {
+ public:
+  int largestInteger(int num) {
+    const string s = to_string(num);
+    int ans = 0;
+    vector<priority_queue<int>> maxHeap(2);
+
+    for (const char c : s) {
+      const int digit = c - '0';
+      maxHeap[digit % 2].push(digit);
+    }
+
+    for (const char c : s) {
+      const int i = c - '0' & 1;
+      ans *= 10;
+      ans += maxHeap[i].top(), maxHeap[i].pop();
+    }
+
+    return ans;
+  }
+};
